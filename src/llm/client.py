a364436c446eca0b167d711c3571c80f040bbfde
Jonathan Reshef <jonathan.reshef@altaml.com>
2025-11-14 16:10:13 -0700

    4.1 mini added as a config

--- src/llm/client.py
+++ src/llm/client.py
@@ -1,15 +1,19 @@
 """Azure OpenAI client wrapper"""
 
 import os
-from langchain_openai import AzureChatOpenAI
 from functools import lru_cache
+
 from dotenv import load_dotenv
+from langchain_openai import AzureChatOpenAI
+from typing_extensions import Literal
 
 load_dotenv()
 
 
 @lru_cache(maxsize=1)
-def get_llm_client() -> AzureChatOpenAI:
+def get_llm_client(
+    type: Literal["gpt5-full", "gpt4.1-mini"] = "gpt5-full",
+) -> AzureChatOpenAI:
     """
     Get Azure OpenAI client (cached singleton).
 
@@ -27,13 +31,23 @@ def get_llm_client() -> AzureChatOpenAI:
     if not endpoint:
         raise ValueError("AZURE_LLM_ENDPOINT environment variable not set")
 
-    llm = AzureChatOpenAI(
-        api_key=api_key,  # type: ignore
-        azure_endpoint=endpoint,  # type: ignore
-        azure_deployment="gpt-5",  # type: ignore
-        api_version="2024-08-01-preview",  # type: ignore
-        verbose=False,
-        reasoning_effort="low",
-    )
+    if type == "gpt5-full":
+        llm = AzureChatOpenAI(
+            api_key=api_key,  # type: ignore
+            azure_endpoint=endpoint,  # type: ignore
+            azure_deployment="gpt-5",  # type: ignore
+            api_version="2024-08-01-preview",  # type: ignore
+            verbose=False,
+            reasoning_effort="low",
+        )
+    elif type == "gpt4.1-mini":
+        llm = AzureChatOpenAI(
+            api_key=api_key,  # type: ignore
+            azure_endpoint=endpoint,  # type: ignore
+            azure_deployment="gpt-4-1-mini",  # type: ignore
+            api_version="2024-08-01-preview",  # type: ignore
+            verbose=False,
+            temperature=0,
+        )
 
     return llm
