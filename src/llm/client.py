19fd3f299fce642ee949683a7c3a722d6211e1f3
Jonathan Reshef <jonathan.reshef@altaml.com>
2025-11-22 11:36:04 -0700

    Fixed 4.1-mini loading issue

--- src/llm/client.py
+++ src/llm/client.py
@@ -46,7 +46,7 @@ def get_llm_client(
         llm = AzureChatOpenAI(
             api_key=api_key,  # type: ignore
             azure_endpoint=endpoint,  # type: ignore
-            azure_deployment="gpt-4-1-mini",  # type: ignore
+            azure_deployment="gpt-4.1-mini",  # type: ignore
             api_version="2024-08-01-preview",  # type: ignore
             verbose=False,
             temperature=0,
